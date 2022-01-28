bf25ff092c5c9632482b7967fb63dd9e20a9da10
Alexon1999 <alexon1999@gmail.com>
2022-01-28 14:19:52 +0100

    send index.html

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.sendFile("./public/index.html");
+  res.sendFile("index.html");
 });
 
 app.post("/login", async (req, res) => {
