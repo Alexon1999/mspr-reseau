896dd50deac56ee8a3ef06d5d170cb1800b06ee2
Alexon1999 <alexon1999@gmail.com>
2022-02-18 13:39:29 +0100

    ajouter 2FA

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 const { authenticate } = require("ldap-authentication");
+const twofactor = require("node-2fa");
 const express = require("express");
+const { connectDB } = require("./db");
+const { getUserByName, saveUser } = require("./db/query");
+
+const db = connectDB();
+
 const app = express();
 
 app.use(express.static("./public"));
@@ -37,8 +43,39 @@ app.post("/login", async (req, res) => {
       user: null,
     });
 
+  // bdd pour 2FA
+  let userRetrieved = await getUserByName(db, user.user.displayName);
+  if (!userRetrieved) {
+    const newSecret = twofactor.generateSecret({
+      name: "MSPR",
+      account: user.user.displayName,
+    });
+    const data = {
+      name: user.user.displayName,
+      secret: newSecret.secret,
+      uri: newSecret.uri,
+      qr: newSecret.qr,
+    };
+    userRetrieved = await saveUser(db, data);
+  }
+
+  console.log({ userRetrieved });
+
   res.json({ user });
 });
+
+// app.get("/token", (req, res) => {
+//   const newToken = twofactor.generateToken(newSecret.secret);
+//   console.log(newToken);
+//   res.json(newToken);
+// });
+
+// app.get("/verify/token/:userId/:token", (req, res) => {
+//   const token = req.params.token;
+//   data = twofactor.verifyToken(newSecret.secret, token);
+//   console.log(data);
+//   res.json(data);
+// });
 
 app.use((req, res) => {
   res.status(404).json({
@@ -46,6 +83,7 @@ app.use((req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running in port 5000");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server running in port " + PORT);
 });
---
+++ server/db/query.js
@@ -0,0 +1,31 @@
+async function getUserByName(db, name) {
+  return new Promise((resolve, reject) => {
+    let sql = `SELECT *
+           FROM user
+           WHERE name = ?`;
+
+    db.get(sql, [name], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        resolve(null);
+      }
+      resolve(row);
+    });
+  });
+}
+
+async function saveUser(db, data) {
+  let sql = "INSERT INTO user(name, secret, uri, qr) VALUES (?, ?, ?, ?)";
+
+  db.run(sql, [data.name, data.secret, data.uri, data.qr], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+  });
+  return getUserByName(db, data.name);
+}
+
+module.exports = {
+  getUserByName,
+  saveUser,
+};
